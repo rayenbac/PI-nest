61b8103ad5298d58b11b82eee764e899bb04c945
rayenbac <rayenbac1@gmail.com>
2024-03-05 15:37:53 +0100

    consommation api

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,20 +11,41 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './user/entities/constants';
 import { LoggerMiddleware } from './config/logging.interceptor';
 
-
 @Module({
-  imports: [UserModule, ClientModule, OrderModule, ProductModule, SupplierModule, InvoiceModule,CompanyModule,
+  imports: [
+    UserModule,
+    ClientModule,
+    OrderModule,
+    ProductModule,
+    SupplierModule,
+    InvoiceModule,
+    CompanyModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: jwtConstants.secret, 
-      signOptions: { expiresIn: '1h' }, 
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
     }),
-
   ],
-
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*'); // Assuming your LoggerMiddleware is applied to all routes
+
+    consumer
+      .apply((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Update with your Angular app's URL
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
+
+        if (req.method === 'OPTIONS') {
+          res.sendStatus(204);
+        } else {
+          next();
+        }
+      })
+      .forRoutes('*'); // Apply CORS middleware to all routes
   }
 }
