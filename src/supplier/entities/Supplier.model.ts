0f40badc812c9d9d5064b5e510253f667eb40483
rayenbac <rayenbac1@gmail.com>
2024-04-04 14:08:28 +0200

    last merge

--- src/supplier/entities/Supplier.model.ts
+++ src/supplier/entities/Supplier.model.ts
@@ -2,13 +2,17 @@ import * as mongoose from 'mongoose';
 
 export const SupplierSchema = new mongoose.Schema({
   nameS:{ type: String, required: true },
-  contactInfo: { type: String, required: true },
+  address : { type: String, required: true },
+  phoneNumber : { type: Number, required: true },
+  productType : { type: String, required: true },
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }], 
 });
 
 export interface Supplier extends mongoose.Document {
   nameS: string;
-  contactInfo: string;
+  address: string;
+  phoneNumber: number;
+  productType: string;
   products: mongoose.Types.ObjectId[];
 }
 
--- src/supplier/entities/supplier.dto.ts
+++ src/supplier/entities/supplier.dto.ts
@@ -4,13 +4,16 @@ import mongoose from "mongoose";
 
 export class CreateSupplierDto {
     readonly nameS: string;
-    readonly contactInfo: string;
+    readonly address: string;
+    readonly phoneNumber: number;
+    readonly productType: string;
     products: mongoose.Types.ObjectId[];
   }
   
   export class UpdateSupplierDto {
     readonly nameS?: string;
-    readonly contactInfo?: string;
+    readonly address?: string;
+    readonly phoneNumber?: number;
+    readonly productType?: string;
     readonly products?: string[];
   }
-  
